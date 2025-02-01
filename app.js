5fc6db344ea2d80252afa266cfefb96bcd40899e
Jorge Mendoza <jlmendoza.isp2@gmail.com>
2025-02-01 18:41:32 -0300

    Añade header y estilos a index.html

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Ejecutando el Sistema");
+const hamburger = document.querySelector("#hamburguesa");
+const enlaces= document.querySelector('#nav__links');
+
+hamburger.addEventListener('click',() => {
+    enlaces.classList.toggle('show')
+});
