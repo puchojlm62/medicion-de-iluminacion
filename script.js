c19dcca85389f19440b77b5f4dab9f0260cdd8f8
Jorge Mendoza <jlmendoza.isp2@gmail.com>
2025-02-05 19:34:06 -0300

    Añadir cambios en app.js index.html mediciones.htm y script.js para realizar mejoras en footer

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function toggleMenu() {
     menu.classList.toggle("activo");
 }
 
+/* para abrir y cerrar el modal */
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("modalAcerca");
+    const openModal = document.getElementById("openModal");
+    const closeModal = document.querySelector(".close");
+
+    // Abrir el modal al hacer clic en el enlace
+    openModal.addEventListener("click", function (event) {
+        event.preventDefault(); // Evita que el enlace recargue la página
+        modal.style.display = "block";
+    });
+
+    // Cerrar el modal al hacer clic en la "X"
+    closeModal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    // Cerrar el modal si el usuario hace clic fuera del contenido
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
